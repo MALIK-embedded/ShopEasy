e2dffbdcac8fa5090826995cec501adb1570bd6f
Malik_embedded <syedmaliksameer001@gmail.com>
2026-07-24 13:03:28 +0530

    Day 3: Added ShopEasy login system with credential validation

---
+++ python/day3/login_system.py
@@ -0,0 +1,21 @@
+print("=" * 50)
+print("               WELCOME TO SHOPEASY")
+print("=" * 50)
+username = "Malik"
+passkey = "Malik@123"
+name = input("Enter Username : ")
+password = input("Enter password : ")
+if name == username and password == passkey :
+    print()
+    print("Login Successful!")
+    print("Welcome To ShopEasy.")
+elif name != username and password == passkey :
+    print()
+    print("Invalid Username.")
+elif name == username and password != passkey :
+    print()
+    print("Incorrect Password.")
+else :
+    print()
+    print("Invalid Username or Password.")
+print("=" * 50)
